795c912cf677bc7f52d51fcb4a169389af774a4f
monkukui <monkukui1953@gmail.com>
2020-09-04 13:05:13 +0900

    Add cli arg

--- cmd/gpt/main.go
+++ cmd/gpt/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
 	"gpt"
+	"log"
 )
 
 func main() {
-	gpt.Generate("testdata/src/a/a.go", "testdata/src/a/lib")
+	mainFilePath := flag.String("main", "", "main file path")
+	libDirPath := flag.String("lib", "", "lib dir path")
+	flag.Parse()
+
+	if *mainFilePath == "" || *libDirPath == "" {
+		log.Print("Usage: ./gpt -main <main_file_path> -lib <library_dir_path>")
+		return
+	}
+
+	gpt.Generate(*mainFilePath, *libDirPath)
 }
